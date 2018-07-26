bcfb8033cf1a3d8167fe7358252101dee1cd5cc7
CurlyCuteLauren <lnziegele@gmail.com>
2018-07-26 16:20:13 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -1,18 +1,35 @@
-
 def create_olympics_hash
+olympic_places_array= ["Sydney", "Athens", "Beijing", "London"]
+olympic_years_array = [2000, 2004, 2008, 2012]
+olympic_places_array.each do |places|
+olympics_hash[places]= olympic_years_array[years]
+olympics_hash = {}
+places_year = 0 
+
+  olympics_hash[places]= olympic_years_array[years]
+  places_year += 1
+end 
+return olympics_hash
+puts olympics_hash
+puts create_olympics_hash
+puts "The #{years} summer olympics took place in #{places}."
+
+    
+  
+
+  
   # Implement this method so that it returns a hash with the data provided on README.md
-end
 
-def add_a_key_value_pair
+
+ #add_a_key_value_pair
   # Implement this method so that it adds a key value pair to the hash created in create_olympics_hash
-end
 
-def iterate_through_hash
+
+ #iterate_through_hash
   # Implement this method so that it iterates over the hash created in add_a_key_value_pair
   # and puts each key value pair within the phrase "The _____ summer olympics took place in _____."
-end
 
-def iterate_through_keys
-  # Implement this method so that it converts the keys of the hash created in add_a_key_value_pair
-  # into uppercased Strings, creates a new array with these values, and then puts each upcase key in the CLI
+#iterate_through_keys
+#Implement this method so that it converts the keys of the hash created in add_a_key_value_pair
+#into uppercased Strings, creates a new array with these values, and then puts each upcase key in the CLI
 end
